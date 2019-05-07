b1ac2db42c3e4bc3251d8ce05f37de44ed1eb49d
Karin Povolozki-Rabichev <karinpovolozki512@gmail.com>
2019-05-07 19:55:28 +0100

    Initial files

---
+++ script.js
@@ -0,0 +1,143 @@
+// 'use strict';
+ // const starDetail = require('./starDetail');
+
+window.onload = function() {
+	const ul = document.querySelector('ul.solarsystem');
+	const sun = document.querySelector('li.sun');
+	const description = document.querySelector('ul#descriptions');
+	let planets = [] ;
+
+	let x = fetch('https://swapi.co/api/planets/')
+  		.then( rsp => rsp.json())
+  		.then( res => {
+  			let planets = res.results;
+
+		const addStar = (arr) => {
+
+			const newPlanet = arr.map((planet , index) => {
+			const li = document.createElement('li');
+			li.setAttribute("class" , `star planet-${index}`);
+
+
+			li.style.width = `100px + ${index}`;
+			ul.appendChild(li)
+
+			const a = document.createElement('a');
+			a.setAttribute("href" , `#${planet.name}`);
+			const span = document.createElement('span');
+			span.appendChild(document.createTextNode(`${planet.name}`));
+
+			const reduceSize = (num) => {
+				let size = Math.floor((num/1000)/1.5);
+				if (size > 10) {
+					return (size / 10);
+				} else if (size < 2) {
+					return (size * 2)
+				}
+				return size
+			} 
+			const starWidth = reduceSize(Number(`${planet.diameter}`));
+			const starHeight = reduceSize(Number(`${planet.diameter}`));
+			const randomColor = Math.floor(Math.random()*16777215).toString(16);
+
+			span.style.width = `${starWidth}px`;
+			span.style.height = `${starHeight}px`;
+			span.style.backgroundColor = `#${randomColor}`;
+			
+			a.appendChild(span);
+			li.appendChild(a);
+		
+			const planetStyle = document.querySelector(`.planet-${index}`);
+			
+
+			const width = 100 +  Number(`${index}`*60);
+			const height = 100 +  Number(`${index}`*60);
+			const borderRadius = (Number(`${width}`)/2)+2;
+			const top =  270 -  Number(`${index}`*30);
+			const left =  430 -  Number(`${index}`*30);
+			const zIndex = 99 - Number(`${index}`);
+			const animationDuration = Number(`${planet.orbital_period}`/Number(`${planet.rotation_period}`))
+			
+	 
+			planetStyle.style.width = `${width}px`;
+			planetStyle.style.height = `${height}px`;
+			planetStyle.style.borderRadius = `${borderRadius}px`;
+			planetStyle.style.top = `${top}px`;
+			planetStyle.style.left = `${left}px`;
+			planetStyle.style.zIndex = `${zIndex}`;
+			planetStyle.style.zIndex = `${zIndex}`;
+			planetStyle.style.animationDuration = `${animationDuration}s`;
+
+			const liDes = document.createElement('li');
+			const h2 = document.createElement('h2');
+			h2.setAttribute("id" , `${planet.name}`);
+			h2.appendChild(document.createTextNode(`${planet.name}`));
+			const p = document.createElement('p');
+
+
+
+
+
+
+			const StgToArr = (str) => {
+				const arr = str.split(',')
+
+				return arr;
+			}
+
+			let names = '[]' ;
+
+			const getInfo = async function (urls) {
+
+				const peopleUrl = urls.filter(url => url.includes('people'));
+				const filmsUrl = urls.filter(url => url.includes('films'));
+
+				const names = (await Promise.all(peopleUrl.map((url) => 
+					fetch(url).then(rsp => rsp.json()).then(res => {
+						return res.name 
+					})
+
+				))).toString()
+
+				const films = (await Promise.all(filmsUrl.map((url) => 
+					fetch(url).then(rsp => rsp.json()).then(res => {
+						return res.title 
+					})
+
+				))).toString()
+
+				const information = {names : `${names}` , films : `${films}`}
+
+				description.appendChild(liDes);
+				liDes.appendChild(h2);
+				liDes.appendChild(p);
+
+				const info = `
+					Name: ${planet.name}
+					Rotation Period: ${planet.rotation_period}
+					Orbital Period: ${planet.orbital_period}
+					Climate: ${planet.climate}
+					Gravity: ${planet.gravity}
+					Terrain: ${planet.terrain}
+					Surface Water: ${planet.surface_water}
+					Population: ${planet.population}
+					residents: ${information.names}
+					films: ${information.films}
+				`
+				p.appendChild(document.createTextNode(`${info}`));
+
+			}
+			const newResidents = StgToArr(`${planet.residents}`);
+			const filteredResident = newResidents.filter(Boolean)
+			const newFilms = StgToArr(`${planet.films}`);
+			const filteredFilms = newFilms.filter(Boolean)
+
+	    	const activeUrls = [filteredResident,filteredFilms].filter(arr => arr.length > 0).flat()
+
+			const residents = getInfo(activeUrls)
+				})
+			return  newPlanet;
+		} 
+addStar(planets);
+})
+}
---
+++ starDetail.js
@@ -0,0 +1,49 @@
+export const starDetail = 
+[
+{
+	name: 'Coruscant',
+	rotation_period: '36',
+	orbital_period: '368',
+	diameter: '12240',
+	climate: 'temperate',
+	gravity: '1 standard',
+	terrain: 'cityscape, mountains',
+	surface_water: '0.05',
+	population: '1000000000000',
+	residents: [
+	'https://swapi.co/api/people/34/',
+	'https://swapi.co/api/people/55/',
+	'https://swapi.co/api/people/74/'
+	],
+	films: [
+	'https://swapi.co/api/films/5/',
+	'https://swapi.co/api/films/4/',
+	'https://swapi.co/api/films/6/',
+	'https://swapi.co/api/films/3/'
+	],
+	created: '2014-12-10T11:54:13.921000Z',
+	edited: '2014-12-20T20:58:18.432000Z',
+	url: 'https://swapi.co/api/planets/9/'
+},
+{
+	name: 'Bespin',
+  rotation_period: '12',
+  orbital_period: '5110',
+  diameter: '118000',
+  climate: 'temperate',
+  gravity: '1.5 (surface), 1 standard (Cloud City)',
+  terrain: 'gas giant',
+  surface_water: '0.05',
+  population: '6000000',
+  residents: [
+    'https://swapi.co/api/people/26/'
+  ],
+  films: [
+    'https://swapi.co/api/films/2/'
+  ],
+  created: '2014-12-10T11:43:55.240000Z',
+  edited: '2014-12-20T20:58:18.427000Z',
+  url: 'https://swapi.co/api/planets/6/'
+}
+
+]
